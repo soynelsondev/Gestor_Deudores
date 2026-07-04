08a874ed44358d4738d5f65dc93075fa2d592c65
Nelson dev <soynelsondev@gmail.com>
2026-07-04 16:51:03 -0400

    colores nuevos

--- app/src/main/java/com/example/gestor_deudores/ui/theme/Color.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+//colores de la app
+val fondo = Color(0xFFFFDDD2)
+val fondo2 = Color(0xFF83C5BE)
+val componentes = Color(0xFFE29578)
+val fondo_claro = Color(0xFFEDF6F9)
+val estado = Color(0xFF006D77)
