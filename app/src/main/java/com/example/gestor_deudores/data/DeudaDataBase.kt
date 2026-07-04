015eec39b3f65c5169fa6f15a8f215c54b29a4b0
Nelson dev <soynelsondev@gmail.com>
2026-07-04 15:04:52 -0400

    creado el database

--- app/src/main/java/com/example/gestor_deudores/data/DeudaDataBase.kt
+++ app/src/main/java/com/example/gestor_deudores/data/DeudaDataBase.kt
@@ -1,2 +1,36 @@
 package com.example.gestor_deudores.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [Deudor::class, Deuda::class],
+    version = 1)
+
+abstract class DeudaDataBase: RoomDatabase(){
+
+    abstract fun deudorDao(): DeudorDao
+    abstract fun deudaDao(): DeudaDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: DeudaDataBase? = null
+
+        fun getDatabase(context: Context): DeudaDataBase{
+            return   INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DeudaDataBase::class.java,
+                   "control_deudas_db"
+                )
+                    .build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
