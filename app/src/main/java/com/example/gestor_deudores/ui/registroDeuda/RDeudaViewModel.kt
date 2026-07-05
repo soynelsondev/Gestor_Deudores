5cbfca25efecd7f422d8236cd71a1f3318b57d4b
Nelson dev <soynelsondev@gmail.com>
2026-07-05 01:04:36 -0400

    estados

--- app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
@@ -20,11 +20,32 @@ data class R_DeudaEstado(
 
 class RDeudaViewModel(private val deudaDao: DeudaDao): ViewModel(){
 
-    private val _uiEstado= MutableStateFlow(R_DeudaEstado())
-    val uiEstado: StateFlow<R_DeudaEstado> =  _uiEstado.asStateFlow()
+    private val _uiState= MutableStateFlow(R_DeudaEstado())
+    val uiEstado: StateFlow<R_DeudaEstado> =  _uiState.asStateFlow()
+
+    // Esta función es vital: la llamas apenas abres la pantalla para inyectarle el ID del cliente
+    fun inicializarIdDeudor(id: Int) {
+        _uiState.update { it.copy(idDeudor = id) }
+    }
 
     fun onMontoChange(nuevoMonto: String) {
-        _uiEstado.update { it.copy(monto = nuevoMonto, error = null) }
+        _uiState.update { it.copy(monto = nuevoMonto, error = null) }
+    }
+
+    fun onTipoDeudaChange(nuevoTipo: String) {
+        _uiState.update { it.copy(tipoDeuda = nuevoTipo, error = null) }
+    }
+
+    fun onFechaChange(nuevaFecha: String) {
+        _uiState.update { it.copy(fecha = nuevaFecha, error = null) }
+    }
+
+    fun onDescripcionChange(nuevaDescripcion: String) {
+        _uiState.update { it.copy(descripcion = nuevaDescripcion, error = null) }
+    }
+
+    fun onRolChange(nuevoRol: String) {
+        _uiState.update { it.copy(rol = nuevoRol, error = null) }
     }
 
 
