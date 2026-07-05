5cacd155d3f0eb37f08b1139dbc306f1dbe3e625
Nelson dev <soynelsondev@gmail.com>
2026-07-05 00:21:33 -0400

    update

--- app/src/main/java/com/example/gestor_deudores/ui/Registro/registroViewModel.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/Registro/registroViewModel.kt
@@ -1,3 +1,25 @@
 package com.example.gestor_deudores.ui.Registro
 
+import androidx.lifecycle.ViewModel
+import com.example.gestor_deudores.data.DeudorDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
+
+data class RegistroDeudorUiState(
+    val nombre: String = "",
+    val apellido: String = "",
+    val telefono: String = "",
+    val cedula: String = "",
+    val error: String? = null,
+    // Usamos esto para guardar el ID recién creado y pasarlo a la otra pantalla
+    val nuevoDeudorId: Int? = null
+)
+
+
+class registroViewModel(private val deudorDao: DeudorDao): ViewModel(){
+    private val estado = MutableStateFlow(RegistroDeudorUiState())
+    val iuEstado: StateFlow<RegistroDeudorUiState> = estado.asStateFlow()
+    
+}
