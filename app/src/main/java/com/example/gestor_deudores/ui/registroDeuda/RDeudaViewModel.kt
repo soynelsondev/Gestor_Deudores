d2770b4ddeb525606bf6b0c87e95b974814a10bc
Nelson dev <soynelsondev@gmail.com>
2026-07-05 00:47:46 -0400

    update

--- app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.gestor_deudores.ui.registroDeuda
 
+data class R_DeudaEstado(
+    val montoInicial : Double = 0.0,
+    val montoRestante: Double= 0.0,
+    val tipoDeuda: String = "",
+    val fecha: String = "",
+    val rol: String = "",
+    val descripcion: String = "",
+    val estado : String = "",
+    val error: String? = null,
+)
