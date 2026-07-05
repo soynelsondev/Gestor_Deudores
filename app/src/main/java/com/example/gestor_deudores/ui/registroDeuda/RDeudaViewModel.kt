78e21393f5c61734cc44db24514166e35f846397
Nelson dev <soynelsondev@gmail.com>
2026-07-05 01:13:51 -0400

    listo deuda

--- app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.gestor_deudores.ui.registroDeuda
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gestor_deudores.data.Deuda
 import com.example.gestor_deudores.data.DeudaDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class R_DeudaEstado(
     val idDeudor: Int = 0,
@@ -48,5 +51,47 @@ class RDeudaViewModel(private val deudaDao: DeudaDao): ViewModel(){
         _uiState.update { it.copy(rol = nuevoRol, error = null) }
     }
 
+    fun GuardarDeuda(){
+        val estado = _uiState.value
+
+        // A. Validaciones críticas
+        if (estado.idDeudor == 0) {
+            _uiState.update { it.copy(error = "Error crítico: Cliente no identificado") }
+            return
+        }
+
+        val montoDouble = estado.monto.toDoubleOrNull()
+        if (montoDouble == null || montoDouble <= 0.0) {
+            _uiState.update { it.copy(error = "Ingresa un monto válido mayor a cero") }
+            return
+        }
+
+        if (estado.fecha.isBlank() || estado.tipoDeuda.isBlank()) {
+            _uiState.update { it.copy(error = "La fecha y el tipo de deuda son obligatorios") }
+            return
+        }
+
+        viewModelScope.launch {
+            val nuevaDeuda = Deuda(
+            idDeudor = estado.idDeudor,
+                montoInicial = montoDouble,
+                montoRestante = montoDouble,
+                tipoDeuda = estado.tipoDeuda,
+                fecha = estado.fecha,
+                rol = estado.rol,
+                descripcion = estado.descripcion,
+                estado = "Pendiente" // Estado inicial automático
+            )
+            deudaDao.agregarDeuda(nuevaDeuda)
+
+            _uiState.update { it.copy(guardadoExitoso = true) }
+
+        }
+
+    }
+    // Limpieza tras el éxito
+    fun reiniciarEstadoGuardado() {
+        _uiState.update { it.copy(guardadoExitoso = false) }
+    }
 
 }
