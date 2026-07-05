379b8de3d0dffdbe67a8c3464d978b06e9a37916
Nelson dev <soynelsondev@gmail.com>
2026-07-05 00:55:43 -0400

    Update

--- app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
+++ app/src/main/java/com/example/gestor_deudores/ui/registroDeuda/RDeudaViewModel.kt
@@ -1,12 +1,28 @@
 package com.example.gestor_deudores.ui.registroDeuda
 
+import androidx.lifecycle.ViewModel
+import com.example.gestor_deudores.data.DeudaDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 data class R_DeudaEstado(
-    val montoInicial : Double = 0.0,
-    val montoRestante: Double= 0.0,
+    val idDeudor: Int = 0,
+    val monto : String = "",
     val tipoDeuda: String = "",
     val fecha: String = "",
-    val rol: String = "",
+    val rol: String = "Cliente",
     val descripcion: String = "",
-    val estado : String = "",
     val error: String? = null,
+    val guardadoExitoso: Boolean = false
 )
+
+class RDeudaViewModel(private val deudaDao: DeudaDao): ViewModel(){
+
+    private val _uiEstado= MutableStateFlow(R_DeudaEstado())
+    val uiEstado: StateFlow<R_DeudaEstado> =  _uiEstado.asStateFlow()
+
+    
+
+
+}
